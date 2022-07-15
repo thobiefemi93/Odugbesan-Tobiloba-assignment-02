912356def20141c52f09267ec4bd379967d9fd16
Tobiloba Odugbesan <109253969+thobiefemi93@users.noreply.github.com>
2022-07-15 12:26:08 +0000

    challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
+  let sum = 0;
+  for (let i = 0; i<arrayOfNumbers.length; i++) {
+    sum=sum +arrayOfNumbers[i]
+  }
+  return sum;
+  }
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +32,13 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.countEvenNumbers = countEvenNumbers;
+  let result =0;
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    if ((arrayOfNumbers[i]%2)=== 0){result+=1;}
+  }
+  return result;
+  }
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
